35165606837e443684ff6565552c974868211da3
Yari <narducciyarigmail.com>
2023-01-09 15:25:09 +0100

    modifica css

--- index.php
+++ index.php
@@ -4,16 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <link rel="stylesheet" href="css/style.css">
+
     <title>BadWord</title>
+
 </head>
+
 <body>
+
      <header>
+
           <h1>
                BAD WORD
           </h1>
+
      </header>
-   <div class="container">
+
+   <main class="container">
+
    <?php
 
 // Creiamo la variabile testo che andremo poi a modificare
@@ -55,7 +64,12 @@
 
 
 ?>
-   </div>
+
+   </main>
+
+   <footer>
+     
+   </footer>
    
 </body>
 </html>
